dc610f364b3a5ae756af0f6da62bab07dd9ed8b4
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-05-27 20:06:19 -0300

    Criando construtores

--- Seção 13/146.Enumeracoes/application/Enums/src/entities/Order.java
+++ Seção 13/146.Enumeracoes/application/Enums/src/entities/Order.java
@@ -1,5 +1,23 @@
 package entities;
 
+import java.util.Date;
+import entities.enums.OrderStatus;
+
 public class Order {
+
+    private Integer id;
+    private Date momento;
+    private OrderStatus status;
+
+    public Order() {
+    }
+
+    public Order(Integer id, Date momento, OrderStatus status) {
+        this.id = id;
+        this.momento = momento;
+        this.status = status;
+    }
+
     
+
 }
