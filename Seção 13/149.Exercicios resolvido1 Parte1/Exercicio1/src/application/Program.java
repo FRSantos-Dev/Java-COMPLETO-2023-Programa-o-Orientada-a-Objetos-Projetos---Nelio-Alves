6f39c26b0afa251c2cdf5ac668c6c5e48844862d
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-05-28 17:33:25 -0300

    Finalizando Exercicio

--- Seção 13/149.Exercicios resolvido1 Parte1/Exercicio1/src/application/Program.java
+++ Seção 13/149.Exercicios resolvido1 Parte1/Exercicio1/src/application/Program.java
@@ -1,5 +1,67 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
 public class Program {
-    
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter three numbers: ");
+        String departmentName = sc.nextLine();
+        System.out.print("Enter the worker data: ");
+        System.out.print("Name: ");
+        String workerName = sc.nextLine();
+        System.out.print("Level: ");
+        String workerLevel = sc.nextLine();
+        System.out.print("Base salary: ");
+        double baseSalary = sc.nextDouble();
+        Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary,
+                new Department(departmentName));
+        // 1P-> Instanciou um objeto do tipo Worker
+        // 2P->Nome digitei
+        // 3P->Instanciação da WorkerLevel foi digitado
+        // 4P->BaseSalary foi digitado
+        // 5P->Associado a esse objeto tem um outro objeto do tipo Departamento e o nome
+        // foi digitado.
+
+        System.out.print("How many contracts to this worker? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Enter contract #" + i + " data: ");
+            System.out.print("Date (DD/MM/YYYY): ");
+            Date contractDate = sdf.parse(sc.next());
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+            System.out.print("Duration (hours): ");
+            int hours = sc.nextInt();
+            HourContract contract = new HourContract(contractDate, valuePerHour, hours);// Instanciando contrato
+            worker.addContract(contract);// associando trabalhador com contrato.
+
+        }
+
+        System.out.println();
+        System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+        String monthAndYear = sc.next();
+        int month = Integer.parseInt(monthAndYear.substring(0, 2));
+        int year = Integer.parseInt(monthAndYear.substring(3));
+        System.out.println("Name: " + worker.getName());
+        System.out.println("Department: " + worker.getDepartment().getName());
+        System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
+        System.out.println();
+        System.out.println("Enter the number of hours to calculate the income: ");
+
+        sc.close();
+    }
 }
