97444660fd30d5b5f3579f481a292fdb74798ae3
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-05-28 13:59:00 -0300

    add getters e setters

--- Seção 13/149.Exercicios resolvido1 Parte1/Exercicio1/src/entities/Worker.java
+++ Seção 13/149.Exercicios resolvido1 Parte1/Exercicio1/src/entities/Worker.java
@@ -24,5 +24,45 @@ public class Worker {
         this.baseSalary = baseSalary;
         this.department = department;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(List<HourContract> contracts) {
+        this.contracts = contracts;
+    }
     
 }
