37b3b7e0001491dfc799a4f6024988fbe0790017
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-06-18 18:07:49 -0300

    up

--- Seção 14/161.ExercicioResolvido/exercicioResolvido/src/application/App.java
+++ Seção 14/161.ExercicioResolvido/exercicioResolvido/src/application/App.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 import entities.Employees;
+import entities.OutsourcedEmployee;
 
 public class App{
 
@@ -22,10 +23,26 @@ public class App{
         System.out.println("Funcionário #" + i + "data: ");
         System.out.println("Terceirizado (y/n) ?");
         char ch = sc.next().charAt(0);//Lê o caractere da linha anterior y ou n
+        System.out.print("Nome: ");
+        sc.nextLine();//Lê o nome
+        String name = sc.nextLine();
+        System.out.print("Horas: ");
+        int hours = sc.nextInt();
+        System.out.print("Valor por hora: ");
+        double valuePerHour = sc.nextDouble();
+        if (ch ==  'y'){//se for terceirizado
+            System.out.println("Digite o Custo Adicional: ");
+            double additionalCharge = sc.nextDouble();
+            //Employees emp = new OutsourcedEmployee(name, null, null, null);//Instanciando o funcionário terceirizado
+            //list.add(emp);//adicionando o funcionário na lista
+            list.add(new OutsourcedEmployee(name, null, null, null));
         }
-
-
-
+        else{//se não for terceirizado
+            //Employees emp = new Employees(name, hours, valuePerHour);//Instanciando o funcionário--Classe emp não é necessário, pode ser feito direto.
+            //list.add(emp);//adicionando o funcionário na lista
+            list.add(new Employees(name, null, null))
+        }
+    }
 
         sc.close();
     }
